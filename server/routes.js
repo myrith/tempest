3d6f89f1c59da4423e6b34c55f47901169ff09ef
Jerven Clark Chua <jervenclark@gmail.com>
2016-04-25 17:35:43 +0800

    Add post endpoints

--- server/routes.js
+++ server/routes.js
@@ -7,23 +7,31 @@ module.exports = function(app) {
   let route = express.Router();
 
   route
-    .get('/api/posts', all);
+    .get('/api/posts', (req, res) => {
+      let posts = fs.readdirSync('static').map((post) => {
+        return {
+          date: post.slice(0, 10),
+          title: post.slice(11, post.length - 3).split('-').join(' ')
+        };
+      });
+      res.json(posts);
+    });
 
   route
-    .get('/api/posts/:title', single);
+    .get('/api/posts/:title', (req, res) => {
+      if (req.params.title) {
+        let post = fs.readFileSync(`static/${req.params.title}.md`, 'utf-8');
+        res.json(post)
+      } else {
+        res.status(404).json({
+          message: 'Not found'
+        });
+      }
+    });
 
   route
     .get('/*', express.static('dist'));
 
   app.use(route);
 
-}
-
-function all(req, res) {
-  let posts = fs.readdirSync('static');
-  res.json(posts);
-}
-
-function single(req, res) {
-  res.json({});
-}
+};
