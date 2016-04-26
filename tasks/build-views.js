785528b0ed212fac02bc3db1089ef9fd08779a59
Jerven Clark Chua <jervenclark@gmail.com>
2016-04-26 08:57:05 +0800

    Add view and view build task

--- tasks/build-views.js
+++ tasks/build-views.js
@@ -3,7 +3,7 @@
 module.exports = function(gulp, config, plugins) {
   return function() {
     return gulp.src('src/views/**/*')
-      .pipe(gulp.dest('dist/assets/views'))
+      .pipe(gulp.dest('dist'))
       .pipe(plugins.livereload())
       .pipe(plugins.notify({
         message: 'Views task complete'
