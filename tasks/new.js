2196fd705db65b729431f2210cc04aded8a50f50
Jerven Clark Chua <jervenclark@gmail.com>
2016-04-27 11:28:25 +0800

    Remove commented text

--- tasks/new.js
+++ tasks/new.js
@@ -9,22 +9,6 @@ module.exports = function(gulp, config, plugins) {
     if (argv.name) {
       let file = `static/${moment().format('YYYY-MM-DD-HH-mm')}-${argv.name}.md`;
       fs.writeFileSync(file, '')
-
-      // [{
-      //   type: 'module',
-      //   dist: `src/${argv.name}.js`
-      // }, {
-      //   type: 'test',
-      //   dist: `tests/${argv.name}.spec.js`
-      // }, {
-      //   type: 'task',
-      //   dist: `tasks/test-${argv.name}.js`
-      // }].map((file) => {
-      //   fs.writeFileSync(
-      //     `./${file.dist}`,
-      //     filter(fs.readFileSync(`./misc/${file.type}.tpl`, 'utf8'))
-      //   )
-      // })
     } else {
       console.log('Post name is required.');
     }
