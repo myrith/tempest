b4b278f6eb40a8838874b593f52b6f5c4ec4a3fe
Jerven Clark Chua <jervenclark@gmail.com>
2016-04-29 06:59:05 +0800

    Add configurable app constants

--- src/js/controllers/base.js
+++ src/js/controllers/base.js
@@ -7,15 +7,23 @@
     .controller('BaseController', Controller);
 
   Controller.$inject = [
+    'APP'
   ];
 
   function Controller(
+    APP
   ) {
 
     var vm = this;
 
-    vm.title = "tempest";
-    vm.description = "blog"
+    [
+      'title',
+      'description',
+      'author',
+      'description'
+    ].forEach(function(field) {
+      vm[field] = APP[field];
+    });
 
     return vm;
 
