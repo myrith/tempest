f3fc53be7f807d283adbdfef682c38602f7589eb
Jerven Clark Chua <jervenclark@gmail.com>
2016-04-28 17:00:34 +0800

    Add blogpost date-time

--- src/js/directives/posts.js
+++ src/js/directives/posts.js
@@ -20,7 +20,18 @@
     var vm = this;
 
     $http.get('/api/posts').then(function(res) {
-      vm.list = res.data;
+      vm.list = [];
+
+      res.data.forEach(function (post) {
+        var dateTime = post.title.split('-').slice(0, 5);
+        vm.list.push({
+          title: post.title.slice(17, post.title.length - 3).split('-').join(' '),
+          date: dateTime.slice(0,3).join('-'),
+          time: dateTime.slice(3,5).join(':'),
+          content: post.content
+        })
+      });
+
     });
 
     return vm;
